4f39535dd8f82e7ce50a0db7e024d60ff80c638f
MyPayNepal <link2bj@gmail.com>
2023-01-19 16:55:49 +0545

    role based guard implemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from './roles.decorator';
+import { Role } from './enums/role.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
